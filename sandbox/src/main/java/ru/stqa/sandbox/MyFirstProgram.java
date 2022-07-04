bc06ec14d8110432995691ed638342ef6a62e369
vaiper013 <vaiper013@rambler.ru>
2022-07-05 00:06:05 +0500

    Пункт 4 выполнен ДЗ

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -2,19 +2,10 @@ package ru.stqa.sandbox;
 
 class MyFirstProgram {
     public static void main(String[] args) {
-        Point pt1=new Point();
-        pt1.x1=10.00;
-        pt1.y1=20.00;
+        Point pt=new Point(10.00,20.00,50.40,70.00);
 
-        Point pt2=new Point();
-        pt2.x2=50.00;
-        pt2.y2=70.00;
-        System.out.println(distance(pt1,pt2));
+        System.out.println(pt.distance());
 
     }
-    public static double distance(Point pt1, Point pt2){
-        double d = Math.sqrt((pt2.x2 - pt1.x1) * (pt2.x2 - pt1.x1) + (pt2.y2 - pt1.y1) * (pt2.y2 * pt1.y1));
-        return d;
 
-    }
 }
--- sandbox/src/main/java/ru/stqa/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/sandbox/Point.java
@@ -6,7 +6,18 @@ public class Point {
     public double x2;
     public double y2;
 
+    public Point(double x1, double x2, double y1, double y2) {
+        this.x1=x1;
+        this.x2=x2;
+        this.y1=y1;
+        this.y2=y2;
+    }
 
+    public  double distance(){
+        double d = Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1) * (this.y2 * this.y1));
+        return d;
+
+    }
 
 
 }
