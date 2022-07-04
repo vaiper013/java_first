a708c885a3a2fb8af9cd1f8bb8e801232c72b801
vaiper013 <vaiper013@rambler.ru>
2022-07-04 23:48:19 +0500

    ДЗ 1,2,3 пункт

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -2,6 +2,19 @@ package ru.stqa.sandbox;
 
 class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Point pt1=new Point();
+        pt1.x1=10.00;
+        pt1.y1=20.00;
+
+        Point pt2=new Point();
+        pt2.x2=50.00;
+        pt2.y2=70.00;
+        System.out.println(distance(pt1,pt2));
+
+    }
+    public static double distance(Point pt1, Point pt2){
+        double d = Math.sqrt((pt2.x2 - pt1.x1) * (pt2.x2 - pt1.x1) + (pt2.y2 - pt1.y1) * (pt2.y2 * pt1.y1));
+        return d;
+
     }
 }
