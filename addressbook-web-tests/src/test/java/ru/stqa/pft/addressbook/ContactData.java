fb649882b4cae77f02d1396200d11ec3077d279c
vaiper013 <vaiper013@rambler.ru>
2022-07-13 02:34:32 +0500

    ДЗ Задание №4

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String lastName;
+    private final String allPhones;
+    private final String allEmail;
+
+    public ContactData(String firstName, String lastName, String allPhones, String allEmail) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.allPhones = allPhones;
+        this.allEmail = allEmail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAllPhones() {
+        return allPhones;
+    }
+
+    public String getAllEmail() {
+        return allEmail;
+    }
+}
