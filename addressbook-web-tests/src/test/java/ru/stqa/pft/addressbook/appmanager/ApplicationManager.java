ab9a7001f28a3c9617999ebef9caecbb3245a861
vaiper013 <vaiper013@rambler.ru>
2022-07-20 19:06:14 +0500

    Задание 7

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,9 +1,13 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.remote.BrowserType.FIREFOX;
 
 public class ApplicationManager {
 
@@ -12,9 +16,21 @@ public class ApplicationManager {
     public GroupHelper groupHelper;
     public SessionHelper sessionHelper;
     public ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
 
     public void init() {
+        if (browser == FIREFOX) {
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.CHROME)
+            wd = new ChromeDriver();
+    }
+
+    {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
@@ -38,5 +54,7 @@ public class ApplicationManager {
         return navigationHelper;
     }
 
-    public ContactHelper getContactHelper() { return contactHelper; }
+    public ContactHelper getContactHelper() {
+        return contactHelper;
+    }
 }
