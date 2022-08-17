699f0da4c66bfb6095cd75e29469bd2aa3eefad5
vaiper013 <vaiper013@rambler.ru>
2022-08-17 20:50:54 +0500

    6.5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,12 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.security.acl.Group;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -16,12 +22,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() {
+  public Iterator<Object[]> validGroups() throws IOException {
     List <Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new GroupData().withName("test2").withHeader("test2").withFooter("test2")});
-    list.add(new Object[] {new GroupData().withName("test3").withHeader("test3").withFooter("test3")});
-    list.add(new Object[] {new GroupData().withName("test4").withHeader("test4").withFooter("test4")});
-return list.iterator();
+    BufferedReader reader = new BufferedReader (new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+      }
+    return list.iterator();
   }
 
   @Test (dataProvider = "validGroups")
@@ -45,7 +55,10 @@ return list.iterator();
 //after.sort(byid);
 //Assert.assertEquals(before,after);
 //before.add(group);
-
+//List <Object[]> list = new ArrayList<Object[]>();
+//    list.add(new Object[] {new GroupData().withName("test2").withHeader("test2").withFooter("test2")});
+//    list.add(new Object[] {new GroupData().withName("test3").withHeader("test3").withFooter("test3")});
+//    list.add(new Object[] {new GroupData().withName("test4").withHeader("test4").withFooter("test4")});
 
 
 
