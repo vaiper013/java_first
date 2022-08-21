6e44a37cf6dfd91f02a9c7d82dc9dea3daa8fcab
vaiper013 <vaiper013@rambler.ru>
2022-08-22 03:01:27 +0500

    Д/з13

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewcontactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewcontactCreationTests.java
@@ -1,23 +1,41 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
-
+import ru.stqa.pft.addressbook.model.GroupData;
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class NewcontactCreationTests extends TestBase {
 
-    @Test
-    public void testNewcontactCreation() throws Exception {
+    @DataProvider
+    public Iterator<Object[]> validContacts() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> groups = (List<ContactData>) xstream.fromXML(xml);
+        return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test (dataProvider = "validContacts")
+    public void testNewcontactCreation(ContactData contact)  {
         app.goToCon().homeContact();
         Contacts before = app.goToCon().all();
-        File photo = new File("src/test/resources/stru.png");
-        ContactData contact = new ContactData().
-                withFirstName("Vasiliy").withLastName("Bochkarev").withAllPhones("7777777").withEmail("vaipermail@rambler.ru").withPhoto(photo);
         app.goToCon().create(contact);
         assertThat(app.Group().count(), equalTo(before.size() + 1));
         Contacts after = app.goToCon().all();
@@ -51,3 +69,7 @@ public class NewcontactCreationTests extends TestBase {
 //before.add(contact);
 //contact.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt());
 //assertThat(after.size(), equalTo(before.size() +1));
+
+//File photo = new File("src/test/resources/stru.png");
+//ContactData contact = new ContactData().
+//withFirstName("Vasiliy").withLastName("Bochkarev").withAllPhones("7777777").withEmail("vaipermail@rambler.ru").withPhoto(photo);
