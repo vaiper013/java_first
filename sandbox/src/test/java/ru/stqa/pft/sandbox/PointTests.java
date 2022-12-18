284eb100ce1c3a67248d30aa4981506471e0aed9
vaiper013 <vaiper013@rambler.ru>
2022-12-19 01:15:09 +0500

    2 попытка

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -7,13 +7,7 @@ import org.testng.annotations.Test;
 public class PointTests {
 
 
-    @Test
-    public void testDistance(){
-        Point p1 = new Point (10.00,10.00);
-        Point p2 = new Point (50.00,50.00);
 
-        assert p1.distance(p2) == 5.477225575051661;
-    }
 
     @Test
     public void testDistance2(){
@@ -23,6 +17,14 @@ public class PointTests {
 
 
     }
+
+    // @Test
+    //    public void testDistance(){
+    //        Point p1 = new Point (10.00,10.00);
+    //        Point p2 = new Point (50.00,50.00);
+    //
+    //        assert p1.distance(p2) == 5.477225575051661;
+    //    }
 
 
 }
